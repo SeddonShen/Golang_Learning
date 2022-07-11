91c9c3a1c6859227ccf45e5c44c45e224359d85d
SeddonShen <283481855@qq.com>
2022-07-11 15:22:38 +0800

    new file about base http

---
+++ base/main.go
@@ -0,0 +1,24 @@
+// 一个基础的Http服务
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/h1", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] %q\n", k, v)
+	}
+}
